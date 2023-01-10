eeaf720f37611b4891e506483f69bccb83ff3f03
yactouat <yactouat@hotmail.com>
2023-01-10 22:06:28 +0100

    api server running locally

--- dist/server.js
+++ dist/server.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("I am run on each PR to main created or updated, 2st try");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const API = (0, express_1.default)();
+API.get("/", (req, res) => {
+    res.status(200).json({ msg: "API is up" });
+});
+const server = API.listen(8080, () => {
+    console.log("API server running");
+});
